079a6c712aadfdb6b22a34075c50b9b2c061422a
anmol chaudhary <anmolchaudhary72@gmail.com>
2022-03-15 22:17:34 +0530

    Commented test

--- BaswareAutomation/src/main/java/prRequisition/Sbs.java
+++ BaswareAutomation/src/main/java/prRequisition/Sbs.java
@@ -48,7 +48,7 @@ public class Sbs extends BaseTest {
 	static String prNumber="";
 	static int j=1;
 	
-	@Test
+	@Test     // Anmol Chaudhary
 	@Parameters("Name")
 	public void sbs(String Name) throws InterruptedException, IOException {
 		
